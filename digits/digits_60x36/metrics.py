d22c6c791111e27ae8cdc56ac1835cfd1b22cda3
Bill Leddy <bill@williesworkshop.net>
2023-01-25 10:19:07 -0800

    add digit files

---
+++ digits/digits_60x36/metrics.py
@@ -0,0 +1,15 @@
+"""
+A class to descive the dimensions and maybe some
+other info about the digit image files in this directory
+"""
+
+class metrics:
+    
+    # most of the characters will be WIDTH wide
+    # some narrower chars are listed here.
+    self.WIDTH = 36
+    self.HEIGHT = 60
+    self.DOT_WIDTH = 24
+    self.COLON_WIDTH = 24
+    self.DASH_WIDTH = 30
+    self.ONE_WIDTH = 30
