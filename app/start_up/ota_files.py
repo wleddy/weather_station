31700a33efab60220f507bfe3cc82aaa84e7e42e
Bill Leddy <bill@williesworkshop.net>
2023-09-15 14:27:53 -0700

    A place to list the files to check for update

---
+++ app/start_up/ota_files.py
@@ -0,0 +1,14 @@
+"""Keep a list of the files that need to be checked
+for OTA updates here."""
+
+import os
+
+def get_ota_file_list():
+    return [
+        'main.py',
+        'lib/senko/check_for_updates.py',
+        'lib/weather_station/weather_station.py',
+        'lib/bmx.py',
+        'lib/ntp_clock.py',
+        'lib/web_client.py',
+        ]
