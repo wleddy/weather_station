fc9b604b25e5b114dc306385eac1c63e476caab7
Bill Leddy <bill@williesworkshop.net>
2023-09-15 14:28:33 -0700

    break checks up into more managable chunks

--- app/lib/senko/check_for_updates.py
+++ app/lib/senko/check_for_updates.py
@@ -14,20 +14,48 @@ def run():
     settings.wlan = Wifi_Connect()
     settings.wlan.connect()
     
-    i = 0
-    while not settings.wlan.is_connected():
-        i += 1
-        time.sleep(1)
-        if i >= 10:
-            return
+    if not settings.wlan.is_connected():
+        return
         
     OTA = senko.Senko(
         user="wleddy", repo="weather_station",
-        files=['main.py','senko/check_for_updates.py','senko/senco.py',]
+        files=[]
         )
-
+    
+    # Seems to fail if I try to check more than 3 files in a block
+    from start_up.ota_files import get_ota_file_list
+    master_list = get_ota_file_list()
+    
+    # break up the master list into managable chunks
+    file_sets = []
+    while master_list:
+        temp_list = []
+        for i in range(2):
+            if len(master_list) > 0:
+                temp_list.append(master_list.pop(0))
+        if temp_list:
+#             print('temp_list:',temp_list)
+            file_sets.append(temp_list)
+            
+    print("Checking for updates")
+#     print('file_sets:',file_sets)
+    changed_files = []
+    for set in file_sets: # a list of lists
         
-    if OTA.update():
+        print('Checking set:',set)
+        OTA.files = set
+        changed_files.append(OTA._check_all())
+        print('{} Changes'.format(len(changed_files)))
+          
+    has_changes = False
+    for set in changed_files: # this is a list of list
+        if set:
+            OTA.files = set
+            has_changes = True
+            print('Updating:',OTA.files)
+            OTA.update()
+        
+    if has_changes:
         print("Updated to the latest version! Rebooting...")
         machine.reset()
     else:
