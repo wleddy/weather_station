c0392436ed5a890fd07ce0c521e8298849fd00b2
Bill Leddy <bill@williesworkshop.net>
2023-09-27 13:58:05 -0700

    Add senko.py to the update list

--- app/start_up/ota_files.py
+++ app/start_up/ota_files.py
@@ -7,6 +7,7 @@ def get_ota_file_list():
     return [ 'start_up/ota_files.py', # Always include this one
         'start_up/time_functions.py',
         'main.py',
+        'lib/senko/senko.py'
         'lib/senko/check_for_updates.py',
         'lib/weather_station/weather_station.py',
         'lib/bmx.py',
