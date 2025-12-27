589d156664efa86e26481ee95c368896787462c4
Bill Leddy <bill@williesworkshop.net>
2025-12-26 17:51:07 -0800

    Changes to be committed: 	modified:   app/settings/time_functions.py
    Update daylight savings times for 26 - 28

--- app/settings/time_functions.py
+++ app/settings/time_functions.py
@@ -7,7 +7,7 @@ class Time_Functions:
 
     def __init__(self):
         # time zone info to adjust UTC time to local time
-        self.time_zone_offset = settings.UTC_offset # the adjustment for standard time
+        self.time_zone_offset = -8 # the adjustment for standard time
         self.daylight_saving_periods = []
         self.daylight_period_setup()
 
@@ -15,36 +15,42 @@ class Time_Functions:
     def daylight_period_setup(self):
         """Set up the start and end times for DST.
         Results are in UTC time."""
-
-        # for 2023
-        #DTS begins Sunday, March 12, 2023, 2:00:00 am
-        #DTS ends Sunday, November 5, 2023, 2:00:00 am
-        self.daylight_saving_periods.append((
-            (time.mktime((2023,3,12,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
-            (time.mktime((2023,11,5,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
-            ))
-        # for 2024
-        #DTS begins Sunday, March 10, 2024, 2:00:00 am
-        #DTS ends Sunday, November 3, 2024, 2:00:00 am
-        self.daylight_saving_periods.append((
-            (time.mktime((2024,3,10,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
-            (time.mktime((2024,11,3,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
-            ))
         # for 2025
         #DTS begins Sunday, March 9, 2025, 2:00:00 am
         #DTS ends Sunday, November 2, 2025, 2:00:00 am
         self.daylight_saving_periods.append((
-            (time.mktime((2025,3,9,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
-            (time.mktime((2025,11,2,2,0,0,0,0)) + (3600 * self.time_zone_offset)),
+            (time.mktime((2025,3,9,2,0,0,0,0))),
+            (time.mktime((2025,11,2,2,0,0,0,0))),
             ))
-        
+        # for 2026
+        #DTS begins Sunday, March 8, 2026, 2:00:00 am
+        #DTS ends Sunday, November 1, 2026, 2:00:00 am
+        self.daylight_saving_periods.append((
+            (time.mktime((2025,3,8,2,0,0,0,0))),
+            (time.mktime((2025,11,1,2,0,0,0,0))),
+            ))
+        # for 2027
+        #DTS begins Sunday, March 14, 2027, 2:00:00 am
+        #DTS ends Sunday, November 7, 2027, 2:00:00 am
+        self.daylight_saving_periods.append((
+            (time.mktime((2027,3,14,2,0,0,0,0))),
+            (time.mktime((2027,11,7,2,0,0,0,0))),
+            ))
+        # for 2028
+        #DTS begins Sunday, March 12, 2028, 2:00:00 am
+        #DTS ends Sunday, November 5, 2028, 2:00:00 am
+        self.daylight_saving_periods.append((
+            (time.mktime((2028,3,12,2,0,0,0,0))),
+            (time.mktime((2028,11,5,2,0,0,0,0))),
+            ))
+       
 
     @property
     def is_daylight_savings(self):
         # Return True if it is daylight savings time else false
         time_seconds = time.mktime(time.localtime())
         # convert to UTC time
-        time_seconds += (3600 * self.time_zone_offset)
+        #time_seconds += (3600 * self.time_zone_offset)
         
         # check if we are in daylight savings time
         for period in self.daylight_saving_periods:
